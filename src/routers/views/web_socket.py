0d544e898ec8d4da0c874a8ec51e2e662880d78a
yeuMyrrh <manhkhai2606@gmail.com>
2024-06-13 10:47:48 +0700

    add more comment

--- src/routers/views/web_socket.py
+++ src/routers/views/web_socket.py
@@ -3,10 +3,13 @@ import websockets
 
 all_clients = []
 
+# gửi tin nhắn đến tất cả các client web đang kết nối tới server websocket
 async def send_message(message : str):
     for client in all_clients:
         await client.send(message)
 
+
+# setting đối với client đã connect được tới web socket
 async def new_client_connected(client_socket, path):
     print("new client connected!")
     all_clients.append(client_socket)
@@ -16,6 +19,7 @@ async def new_client_connected(client_socket, path):
         print("client sent:", new_message)
         await send_message(new_message)
 
+# khởi chạy server websocket 
 async def start_server():
     print("web socket started")
     await websockets.serve(new_client_connected, "localhost", 12345)
